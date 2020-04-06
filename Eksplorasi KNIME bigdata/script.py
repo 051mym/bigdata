4946c314a7ee3f7babe89527ea36dcd252135862
M Yusuf Mukharom <42793951+051mym@users.noreply.github.com>
2020-04-07 02:13:37 +0700

    sdasd

---
+++ Eksplorasi KNIME bigdata/script.py
@@ -0,0 +1,38 @@
+# list = ["serialno","rt","sporder","puma00","puma10","st","adjinc","pwgtp","agep","cit","citwp05","citwp12","cow","ddrs","dear","deye","dout","dphy","drat","dratx","drem","eng","fer","gcl","gcm","gcr","hins1","hins2","hins3","hins4","hins5","hins6","hins7","intp","jwmnp","jwrip","jwtr","lanx","mar","marhd","marhm","marht","marhw","marhyp05","marhyp12","mig","mil","mlpa","mlpb","mlpcd","mlpe","mlpfg","mlph","mlpi","mlpj","mlpk","nwab","nwav","nwla","nwlk","nwre","oip","pap","relp","retp","sch","schg","schl","semp","sex","ssip","ssp","wagp","wkhp","wkl","wkw","wrk","yoep05","yoep12","anc","anc1p05","anc1p12","anc2p05","anc2p12","decade","dis","drivesp","esp","esr","fod1p","fod2p","hicov","hisp","indp","jwap","jwdp","lanp05","lanp12","migpuma00","migpuma10","migsp05","migsp12","msp","naicsp","nativity","nop","oc","occp02","occp10","occp12","paoc","pernp","pincp","pobp05","pobp12","povpip","powpuma00","powpuma10","powsp05","powsp12","privcov","pubcov","qtrbir","rac1p","rac2p05","rac2p12","rac3p05","rac3p12","racaian","racasn","racblk","racnhpi","racnum","racsor","racwht","rc","sciengp","sciengrlp","sfn","sfr","socp00","socp10","socp12","vps","waob","fagep","fancp","fcitp","fcitwp","fcowp","fddrsp","fdearp","fdeyep","fdoutp","fdphyp","fdratp","fdratxp","fdremp","fengp","fesrp","fferp","ffodp","fgclp","fgcmp","fgcrp","fhins1p","fhins2p","fhins3c","fhins3p","fhins4c","fhins4p","fhins5c","fhins5p","fhins6p","fhins7p","fhisp","findp","fintp","fjwdp","fjwmnp","fjwrip","fjwtrp","flanp","flanxp","fmarhdp","fmarhmp","fmarhtp","fmarhwp","fmarhyp","fmarp","fmigp","fmigsp","fmilpp","fmilsp","foccp","foip","fpap","fpobp","fpowsp","fracp","frelp","fretp","fschgp","fschlp","fschp","fsemp","fsexp","fssip","fssp","fwagp","fwkhp","fwklp","fwkwp","fwrkp","fyoep","pwgtp1","pwgtp2","pwgtp3","pwgtp4","pwgtp5","pwgtp6","pwgtp7","pwgtp8","pwgtp9","pwgtp10","pwgtp11","pwgtp12","pwgtp13","pwgtp14","pwgtp15","pwgtp16","pwgtp17","pwgtp18","pwgtp19","pwgtp20","pwgtp21","pwgtp22","pwgtp23","pwgtp24","pwgtp25","pwgtp26","pwgtp27","pwgtp28","pwgtp29","pwgtp30","pwgtp31","pwgtp32","pwgtp33","pwgtp34","pwgtp35","pwgtp36","pwgtp37","pwgtp38","pwgtp39","pwgtp40","pwgtp41","pwgtp42","pwgtp43","pwgtp44","pwgtp45","pwgtp46","pwgtp47","pwgtp48","pwgtp49","pwgtp50","pwgtp51","pwgtp52","pwgtp53","pwgtp54","pwgtp55","pwgtp56","pwgtp57","pwgtp58","pwgtp59","pwgtp60","pwgtp61","pwgtp62","pwgtp63","pwgtp64","pwgtp65","pwgtp66","pwgtp67","pwgtp68","pwgtp69","pwgtp70","pwgtp71","pwgtp72","pwgtp73","pwgtp74","pwgtp75","pwgtp76","pwgtp77","pwgtp78","pwgtp79","pwgtp80"]
+list = ["insp","rt","serialno","division","puma00","puma10","region","st","adjhsg","adjinc","wgtp","np","type","acr","ags","bath","bdsp","bld","bus","conp","elep","fs","fulp","gasp","hfl","mhp","mrgi","mrgp","mrgt","mrgx","refr","rmsp","rntm","rntp","rwat","rwatpr","sink","smp","stov","tel","ten","toil","vacs","valp","veh","watp","ybl","fes","fincp","fparc","grntp","grpip","hhl","hht","hincp","hugcl","hupac","hupaoc","huparc","kit","lngi","multg","mv","noc","npf","npp","nr","nrc","ocpip","partner","plm","psf","r18","r60","r65","resmode","smocp","smx","srnt","sval","taxp","wif","wkexrel","workstat","facrp","fagsp","fbathp","fbdsp","fbldp","fbusp","fconp","felep","ffsp","ffulp","fgasp","fhflp","finsp","fkitp","fmhp","fmrgip","fmrgp","fmrgtp","fmrgxp","fmvp","fplmp","frefrp","frmsp","frntmp","frntp","frwatp","frwatprp","fsinkp","fsmp","fsmxhp","fsmxsp","fstovp","ftaxp","ftelp","ftenp","ftoilp","fvacsp","fvalp","fvehp","fwatp","fyblp","wgtp1","wgtp2","wgtp3","wgtp4","wgtp5","wgtp6","wgtp7","wgtp8","wgtp9","wgtp10","wgtp11","wgtp12","wgtp13","wgtp14","wgtp15","wgtp16","wgtp17","wgtp18","wgtp19","wgtp20","wgtp21","wgtp22","wgtp23","wgtp24","wgtp25","wgtp26","wgtp27","wgtp28","wgtp29","wgtp30","wgtp31","wgtp32","wgtp33","wgtp34","wgtp35","wgtp36","wgtp37","wgtp38","wgtp39","wgtp40","wgtp41","wgtp42","wgtp43","wgtp44","wgtp45","wgtp46","wgtp47","wgtp48","wgtp49","wgtp50","wgtp51","wgtp52","wgtp53","wgtp54","wgtp55","wgtp56","wgtp57","wgtp58","wgtp59","wgtp60","wgtp61","wgtp62","wgtp63","wgtp64","wgtp65","wgtp66","wgtp67","wgtp68","wgtp69","wgtp70","wgtp71","wgtp72","wgtp73","wgtp74","wgtp75","wgtp76","wgtp77","wgtp78","wgtp79","wgtp80"]
+
+file = open("lol.txt", 'w')
+file.write( 'CREATE TABLE ss13pme ( \n' )
+for x in list:
+    file.write( x +' integer, \n' )
+file.write( ');\n' )
+file.close()
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   passwd="",
+#   database="bigdata_02"
+# )
+
+# mycursor = mydb.cursor()
+
+# sql = "INSERT INTO ss13pme (serialno, rt) VALUES (%s, %s)"
+# val = [
+#   (600,"H",),
+#   (800,"H"),
+#   (1,)
+# ]
+
+# for value in val:
+    
+#     for x in value:
+#         if x == None:
+#             x = "None"
+#     print(value)
+#     # mycursor.execute(sql, value)
+
+# mydb.commit()
+
